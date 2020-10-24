1599d8bb603efe4c68716718b6c078eed1d56226
Andevor <shadow362514@gmail.com>
2020-10-24 19:03:36 +0200

    Logic created

--- Library.Data/KonyvtarRepository.cs
+++ Library.Data/KonyvtarRepository.cs
@@ -1,8 +1,34 @@
-﻿using System;
+﻿using Library.Data.Model;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Library.Data
 {
     public class KonyvtarRepository
     {
+        private readonly konyvtarakContext entities = new konyvtarakContext();
+
+        public void Create(Konyvtarak instance)
+        {
+            entities.Konyvtarak.Add(instance);
+            entities.SaveChanges();
+        }
+
+        public IEnumerable<Konyvtarak> Read()
+        {
+            return entities.Konyvtarak.Select(x => x);
+        }
+
+        public void Update(Konyvtarak instance)
+        {
+            entities.Konyvtarak.Update(instance);
+            entities.SaveChanges();
+        }
+
+        public void Delete(Konyvtarak instance)
+        {
+            entities.Konyvtarak.Remove(instance);
+            entities.SaveChanges();
+        }
     }
 }
--- Library.Logic/KonyvtarLogic.cs
+++ Library.Logic/KonyvtarLogic.cs
@@ -1,8 +1,42 @@
-﻿using System;
+﻿using Library.Data;
+using Library.Data.Model;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Library.Logic
 {
     public class KonyvtarLogic
     {
+        KonyvtarRepository repo;
+        List<Konyvtarak> elements;
+
+        public KonyvtarLogic()
+        {
+            repo = new KonyvtarRepository();
+            elements = repo.Read().ToList();
+        }
+
+        public void Create(Konyvtarak instance)
+        {
+            elements.Add(instance);
+            repo.Create(instance);
+        }
+
+        public IEnumerable<Konyvtarak> Read()
+        {
+            return elements;
+        }
+
+        public void Update(Konyvtarak instance)
+        {
+            elements[elements.IndexOf(elements.Find(x => x.Id == instance.Id))] = instance;
+            repo.Update(instance);
+        }
+
+        public void Delete(Konyvtarak instance)
+        {
+            elements.Remove(instance);
+            repo.Delete(instance);
+        }
     }
 }
